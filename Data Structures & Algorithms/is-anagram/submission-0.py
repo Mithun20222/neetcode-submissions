987dccd39947daec21c533ad12742f48eaaaa242
Mithun <155316976+Mithun20222@users.noreply.github.com>
2026-07-05 01:53:32 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a = list(s)
+        a.sort()
+        b = list(t)
+        b.sort()
+        if(len(a)!=len(b)):
+            return False
+        for i in range(len(a)):
+            if(a[i]!=b[i]):
+                return False
+        return True
